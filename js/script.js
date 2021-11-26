28f9383883032ce7071dd41b4059ff6d5589a156
Francesco <scovascova@gmail.com>
2021-11-26 17:39:06 +0100

    array di immagini laterali corretto

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ const app = new Vue({
     el: '#root',
     data: {
         images: [
-            "../img/01.jpg",
-            "../img/02.jpg",
-            "../img/03.jpg",
-            "../img/04.jpg",
-            "../img/05.jpg",
+            "img/01.jpg",
+            "img/02.jpg",
+            "img/03.jpg",
+            "img/04.jpg",
+            "img/05.jpg"
         ]
     },
     methods: {
