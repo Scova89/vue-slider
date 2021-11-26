2be2118dd1ea4b9c11636d1634633535a1d54448
Francesco <scovascova@gmail.com>
2021-11-26 17:06:43 +0100

    array delle img nei data

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-const app = new Vue(
+const app = new Vue({
     el: '#root',
-    
-)
+    data: {
+        images: [
+            "img/01.jpg",
+            "img/02.jpg",
+            "img/03.jpg",
+            "img/04.jpg",
+            "img/05.jpg"
+        ]
+    },
+    methods: {
+
+    }
+})
